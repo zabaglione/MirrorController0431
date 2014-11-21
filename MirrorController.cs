fa74380d183ea253b5a1e8e5adfc165a6024a5b9
zabaglione <z.zabaglione@gmail.com>
2014-11-21 11:21:56 +0900

    初期バージョン

---
+++ MirrorController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ovr;
+
+public class MirrorController : MonoBehaviour {
+
+	public bool mirrorMode;
+
+	// Use this for initialization
+	void Start () {
+		int mirror = PlayerPrefs.GetInt("mirrorMode", -1);
+		if (mirror == -1) {
+			mirror = (mirrorMode == true) ? 1 : 0;
+			PlayerPrefs.SetInt("mirrorMode", mirror);
+			PlayerPrefs.Save();
+		} else {
+			mirrorMode = (mirror == 1) ? true : false;
+		}
+		OVRManager.display.mirrorMode = mirrorMode;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.M)) {
+			mirrorMode = !mirrorMode;
+			int mirror = (mirrorMode == true) ? 1 : 0;
+			PlayerPrefs.SetInt("mirrorMode", mirror);
+			PlayerPrefs.Save();
+		}
+	}
+}
